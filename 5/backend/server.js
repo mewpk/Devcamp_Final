d08191d29293bd169da95c15c16d36bfb30f04fe
KaWaIINa <81364308+millove123@users.noreply.github.com>
2022-06-07 14:23:42 +0700

    success 5

--- 5/backend/server.js
+++ 5/backend/server.js
@@ -10,34 +10,28 @@ app.use(cors())
 
 
 app.get("/api/user", async (req, res) => {
-
+try {
     const connection = await mysql.createConnection({
         host: 'localhost',
         user: 'root', // <== ระบุให้ถูกต้อง
         password: '1234', // <== ระบุให้ถูกต้อง
-        database: 'final', // <== ระบุ database ให้ถูกต้อง
+        database: 'exam', // <== ระบุ database ให้ถูกต้อง
         port: 3306, // <== ใส่ port ให้ถูกต้อง (default 3306, MAMP ใช้ 8889)
 
     })
-    try {
-        const id = await connection.execute(
-            `SELECT * FROM user WHERE 1`
-        )
-        console.log(id[0][0]);
-        (id[0][0]) ? console.log("have") : console.log("noting");
 
-        res.json({
-            message: id[0],
-            status: "OK"
-        }).status(200);
-        await connection.end()
-    } catch (error) {
-        res.json({
-            message: error,
-            status: ";-;"
-        }).status(200);
-        await connection.end()
-    }
+    const join = await connection.execute(
+        `SELECT employee.id,employee.firstname, employee.lastname, job.job_name FROM employee INNER JOIN job ON employee.job_id = job.id;`
+    )
+    await connection.end();
+
+    res.json({
+        data : join[0]
+    })
+
+} catch (error) {
+    
+}
 
 
 })
--- 5/frontend/src/App.js
+++ 5/frontend/src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import { Space, Table, Tag } from 'antd';
+import React, { useEffect, useState } from 'react';
+import 'antd/dist/antd.css'
+import axios from 'axios';
 
-function App() {
+
+export default function App() {
+
+  const [data , setData ] = useState(null)
+  const getData = async ()=>{
+    const data = await axios.get("http://localhost:3000/api/user")
+    setData(data.data.data);
+  }
+
+  useEffect(()=>{
+    if (!data){
+      getData()
+    }
+    else{
+      console.log(data);
+    }
+    
+  })
+
+
+  const columns = [
+    {
+      title: 'Id',
+      dataIndex: 'id',
+      key: 'id',
+    },
+    {
+      title: 'Firstname',
+      dataIndex: 'firstname',
+      key: 'firstname',
+    },
+    {
+      title: 'Lastname',
+      dataIndex: 'lastname',
+      key: 'lastname',
+    },
+    {
+      title: 'Job_name',
+      key: 'job_name',
+      dataIndex: 'job_name',
+      // render: 
+    },
+    {
+      title: 'Photo',
+      key: 'action',
+      render: (x)=>(
+        <>
+        <img src={require("./assets/img/"+x.job_name+'.jpg')} />
+       
+        </>
+      )
+    },
+  ];
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div style={{width :"80%" , margin : "100px auto"}}>
+    
+    <Table columns={columns} dataSource={data}  pagination={true}  rowKey={(dt) => dt.id}/>
+  
+    </div>)
 }
-
-export default App;
