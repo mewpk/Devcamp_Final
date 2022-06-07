cde2d23b025b27cb79c4e2763d5af763da3ce124
KaWaIINa <81364308+millove123@users.noreply.github.com>
2022-06-07 20:26:21 +0700

    clone

---
+++ 6/frontend/src/App.js
@@ -0,0 +1,125 @@
+import React from 'react'
+import 'antd/dist/antd.css';
+import { Button, Checkbox, Form, Input } from 'antd';
+import axios from "axios"
+
+const App = () => {
+  const onFinish = async (values) => {
+    const sendData = await axios.post("http://localhost:3000/api/user", values);
+    if (sendData.data.status === ';-;') {
+      
+      alert("USERNAME ALREADY : Try Again !!")
+    }
+    else{
+      alert('Created !!');
+    }
+    
+  };
+
+  const onFinishFailed = (errorInfo) => {
+    console.log('Failed:', errorInfo);
+  };
+
+  return (
+    <div style={{ width : "80%" , marginTop : "100px"}}>
+    <Form
+      name="basic"
+      labelCol={{
+        span: 8,
+      }}
+      wrapperCol={{
+        span: 16,
+      }}
+      initialValues={{
+        remember: true,
+      }}
+      onFinish={onFinish}
+      onFinishFailed={onFinishFailed}
+      autoComplete="off"
+    >
+      <Form.Item
+        label="Username"
+        name="username"
+        rules={[
+          {
+            required: true,
+            message: 'Please input your username!',
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item
+        label="Password"
+        name="password"
+        rules={[
+          {
+            required: true,
+            message: 'Please input your password!',
+          },
+        ]}
+      >
+        <Input.Password />
+      </Form.Item>
+
+      <Form.Item
+        label="Firstname"
+        name="firstname"
+        rules={[
+          {
+            required: true,
+            message: 'Please input your firstname!',
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+      <Form.Item
+        label="Lastname"
+        name="lastname"
+        rules={[
+          {
+            required: true,
+            message: 'Please input your lastname!',
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+      <Form.Item
+        label="Email"
+        name="email"
+        rules={[
+          {
+            required: true,
+            message: 'Please input your email !',
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+      <Form.Item
+        label="Mobile Phone"
+        name="mobile_phone"
+      >
+        <Input />
+      </Form.Item>
+
+     
+
+      <Form.Item
+        wrapperCol={{
+          offset: 8,
+          span: 16,
+        }}
+      >
+        <Button type="primary" htmlType="submit">
+          Submit
+        </Button>
+      </Form.Item>
+    </Form>
+    </div>);
+};
+
+export default App;
